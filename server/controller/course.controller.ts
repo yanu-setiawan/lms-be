ca9748c7fa4ca82eafc9ac51a9b824c398619f67
yanu-setiawan <yanusetiawan777@gmail.com>
2024-01-03 12:25:39 +0700

    Update User avatar

---
+++ server/controller/course.controller.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middleware/catchAsyncError";
+import ErrorHandler from "../utils/ErrorHandler";
+import cloudinary from "cloudinary";
+
+//upload course
+export const uploadCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
